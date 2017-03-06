20ba548a5e1949cc52f653d342edbc4de9494eeb
robgaston1 <robcoach0221@gmail.com>
2017-03-06 08:45:02 -0500

    Add CountdownForm and some tests to make sure called

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -4,10 +4,10 @@ var Nav = require('Nav');
 var Main = (props) => {
     return (
         <div>
-            <div>
-              <Nav/>
-                <div>
-                  <p>Main.jsx rendered</p>
+          <Nav/>
+            <div className="row">
+                <div className="column small-centered medium-6 large-4">
+
                     {props.children}
                 </div>
             </div>
